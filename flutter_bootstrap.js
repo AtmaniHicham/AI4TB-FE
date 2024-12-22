3ae6778645b069095395181f993be99ca1cb9f52
AtmaniHicham <atmani.hicham.work@gmail.com>
2024-12-22 23:55:46 +0100

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4263031456"
+    serviceWorkerVersion: "2941667015"
   }
 });
